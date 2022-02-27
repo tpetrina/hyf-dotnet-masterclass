99e6be1478377029ef835e11e80dfc54a6bbda08
Hani-far <hfarjamy@outlook.com>
2022-02-27 20:27:47 +0100

    Task2

---
+++ week1/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿string input = "Intellectualization";
+string inputToLowerCase = input.ToLower();
+
+string Vowels= "aeiou";
+char[] VowelsArray = Vowels.ToCharArray();
+
+int GetVowelCount(string inputToLowerCase){
+    int VowelsCount = 0;
+
+    foreach (char letter in inputToLowerCase)
+    {
+        foreach (char vowel in VowelsArray)
+        {
+            if (letter == vowel) VowelsCount++;
+        }
+    }
+    return VowelsCount;
+}
+
+int vowelCount = GetVowelCount(inputToLowerCase);
+Console.WriteLine($"Number of vowels: {vowelCount}");
