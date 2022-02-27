d189460552ee230fc294317e7697e81f3e3b9817
Hani-far <hfarjamy@outlook.com>
2022-02-27 20:28:31 +0100

    Task4

---
+++ week1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4. Classical task
+
+int n = 6;
+int nthNumber = Fibonacci(n);
+
+int Fibonacci(int n){
+var Pre1= 0;
+var Pre2= 1;
+var Sum = 0;
+for (int i = 2; i <= n; i++)
+{
+    Sum = Pre1 + Pre2;
+    Pre1 = Pre2;
+    Pre2 = Sum;
+}
+return Sum;
+}
+Console.WriteLine($"Nth fibonacci number is {nthNumber}");
+
+//int n = 6 => Nth fibonacci number is 8
