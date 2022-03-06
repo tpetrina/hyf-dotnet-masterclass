b50e22aa8ce3d138268213e6b46e4deeeb7dcf58
Hani-far <hfarjamy@outlook.com>
2022-03-06 21:47:57 +0100

    Week2 Home work task 1,2,3 added

---
+++ week2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// 2. Adding constructor
+
+var time = new JupiterTime(7, 40);
+var time1 = new JupiterTime(14, 88);
+PrintTime(time1);
+
+void PrintTime(JupiterTime time)
+{
+    Console.WriteLine($"{time.Hours}:{time.Minutes}");
+} 
+public class JupiterTime
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; } 
+    public JupiterTime(int hours, int minutes){
+        Hours = hours;
+        Minutes=minutes;
+
+        if(Minutes>59){
+            Hours += minutes/60;
+            Minutes = minutes % 60;
+        }
+    }
+}
---
+++ week2/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// 3. Adding time
+
+var time = new JupiterTime(2, 20);
+var timeIn1Hour = time.AddHours(1);
+
+PrintTime(timeIn1Hour);
+
+void PrintTime(string time)
+{
+    Console.WriteLine($"{time}");
+} 
+public class JupiterTime
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; } 
+    public JupiterTime(int hours, int minutes){
+        Hours = hours;
+        Minutes=minutes;
+
+        if(Minutes>59){
+            Hours += minutes/60;
+            Minutes = minutes % 60;
+        }
+    }
+
+    public string AddHours(int hour){
+         Hours +=hour;
+         return ($"{Hours}:{Minutes}");
+    }
+}
